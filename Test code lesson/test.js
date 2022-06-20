38eba2730fa0e42ba676c1b4ac28a2b3bdf78c93
Trungvo93 <100949580+Trungvo93@users.noreply.github.com>
2022-06-20 21:32:44 +0700

    updalte máy bàn

---
+++ Test code lesson/test.js
@@ -0,0 +1,6 @@
+function letter(){
+    let name = document.getElementById("name").value;
+    let address = document.getElementById("address").value;
+    let date = document.getElementById("date").value;
+    document.getElementById("result").innerHTML = name;
+}
